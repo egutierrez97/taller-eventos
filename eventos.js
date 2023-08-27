c598d97af2391643195e3752a6fdebebbaf1f679
egutierrez97 <141046002+EGutierrez97@users.noreply.github.com>
2023-08-27 16:35:23 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 const divButton = document.querySelector("div");
+const button = document.querySelector("button");
 
 divButton.addEventListener("click", () => {
     alert('Hola! Soy el div');
+});
+
+button.addEventListener("click", (event) => {
+    event.stopPropagation();
 });
